2eeb09148196aa63f55038da68ce883c1537f9d9
Eran Guttentag <>
2021-07-18 07:23:11 +0300

    Map Screen

---
+++ Shared/GeofencerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeofencerApp.swift
+//  Shared
+//
+//  Created by Eran Gutentag on 16/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct GeofencerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Geofencer
+//
+//  Created by Eran Gutentag on 16/07/2021.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
