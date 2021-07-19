f21955476f43fc98e69593111db75fced1930b03
Eran Guttentag <>
2021-07-19 20:46:38 +0300

    Geofence Rule

---
+++ Shared/Rule/GeofenceRule.swift
@@ -0,0 +1,34 @@
+//
+//  GeofenceRule.swift
+//  Geofencer (iOS)
+//
+//  Created by Eran Gutentag on 18/07/2021.
+//
+
+import Foundation
+import CoreLocation
+
+class GeofenceRule {
+    let id: String
+    let name: String
+    let location: CLLocationCoordinate2D
+    let radius: Float
+    
+    init(id: String, name: String, location: CLLocationCoordinate2D, radius: Float) {
+        self.id = id
+        self.name = name
+        self.location = location
+        self.radius = radius
+    }
+}
+
+extension GeofenceRule: Hashable {
+    static func ==(lhs: GeofenceRule, rhs: GeofenceRule) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(location.latitude)
+        hasher.combine(location.longitude)
+    }
+}
